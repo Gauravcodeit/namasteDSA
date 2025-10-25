329d3a91d4a6fe8b4cb79258baa1ee6f7b9e050b
Gauravcodeit <adhikarigaurav82@gmail.com>
2025-10-25 17:07:59 +0530

    practicing the star pattern

--- app.js
+++ app.js
@@ -46,6 +46,10 @@ function smallestInArray(arr){
 console.log(smallestInArray([1,2,-14,-15,0]))
 
 function findSecondlargest(arr){
+    if(arr.length < 2){
+        return "Not required length"
+    }
+
     let largest = -Infinity;
     let secondlargest = -Infinity;
     for(let i=0; i< arr.length; i++){
@@ -54,10 +58,52 @@ function findSecondlargest(arr){
             largest = arr[i];
 
         }
-        else if (arr[i] > secondlargest ){
+        else if (arr[i] > secondlargest && arr[i] != largest){
             secondlargest = arr[i]
         }
     }
     return secondlargest
 }
 console.log(findSecondlargest([-1,-2,-14,-15,-1]), "second largest")
+
+function starpattern1(n){
+    for (let i=0; i< n ; i++){
+        let row= ""
+        for (let j=0; j< i+1 ; j++){
+            row = row + '*'
+        }
+        console.log(row)
+
+    }
+}
+starpattern1(10);
+
+function starpattern2(n){
+    let row,val =0;
+    for (let i=0; i< n ; i++){
+        row= "";
+        val =0;
+        for (let j=0; j< i+1 ; j++){
+            val = j+1;
+            row = row + val
+        }
+        console.log(row)
+
+    }
+}
+starpattern2(5);
+
+function starpattern3(n){
+    let row,val =0;
+    for (let i=0; i< n ; i++){
+        row= "";
+        val =0;
+        for (let j=0; j< i+1 ; j++){
+            val = i+1;
+            row = row + val
+        }
+        console.log(row)
+
+    }
+}
+starpattern3(5);
