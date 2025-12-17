eaed66cdf78c6e2acbf743702b62ee5a942791a3
Gauravcodeit <adhikarigaurav82@gmail.com>
2025-12-18 00:01:43 +0530

    simplest form of recrusion for simmminng

--- app.js
+++ app.js
@@ -542,3 +542,9 @@ function sumOfFirstNNumber(n, sum){
     return sumOfFirstNNumber(n, sum)
 }
 console.log(sumOfFirstNNumber(3,0))
+
+function sum (n){
+    if(n==0) return 0;
+    return (n + sum(n-1))
+}
+console.log(sum(5));
