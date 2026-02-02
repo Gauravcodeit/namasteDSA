a37c0d78da8af8ce26d5e1caa6a64a09bb7f7a17
Gauravcodeit <adhikarigaurav82@gmail.com>
2026-02-02 22:14:36 +0530

    binary seacrh

--- app.js
+++ app.js
@@ -620,3 +620,26 @@ function linearSearch(arr, target){
 console.log(linearSearch([1,2,3],1))
 console.log(linearSearch([1,2,3],3))
 console.log(linearSearch([1,2,3],4))
+
+function binarySearch(arr, target) {
+    let left =0;
+    let right = arr.length -1;
+    let middle;
+    while(left <= right) {
+        middle = Math.floor((left + right )/2)
+        if(arr[middle] == target){
+            return middle
+        }
+        else if(arr[middle] < target) {
+            left = middle + 1;
+        }
+        else {
+            right = middle -1;
+        }
+    }
+
+    return -1
+}
+console.log(binarySearch([1,2,4],1))
+console.log(binarySearch([1,2,4],3))
+console.log(binarySearch([1,2,4],4))
