f5a0d3787f1ced94671a3892e12fc14dd97e1d17
Gauravcodeit <adhikarigaurav82@gmail.com>
2026-04-25 00:29:11 +0530

    added linked list

--- DSA/Linkedlist.js
+++ DSA/Linkedlist.js
@@ -44,13 +44,44 @@ myLinkedList.prototype.addAtIndex = function(index, value) {
     }
     else {
         let curr = this.head;
-        for (let i=0; i < index - 1; i++){
+        for (let i=0; i < index - 1; i++) {
             curr = curr.next;
         }
         newNode.next = curr.next;
         curr.next =newNode;
     }
     this.size++
+}
+myLinkedList.prototype.get = function(index){
+    if (index >= this.size || index < 0) return;
+    curr = this.head;
+    for (let i=0; i < index; i++) {
+        curr = curr.next;
+    }
+    return curr.value;
+}
+
+myLinkedList.prototype.deleteNode = function(index){
+    if (index >= this.size || index < 0) return;
+    if (index == 0) {
+        this.head = this.head.next;
+    }
+    else{
+        let curr = this.head;
+        for (let i =0; i< index -1; i++) {
+            curr = curr.next;
+        }
+         curr.next = curr.next.next;
+    }
+    this.size --;
+}
+
+myLinkedList.prototype.print = function(){
+    let curr = this.head;
+    for (let i = 0; i< this.size; i++) {
+        console.log(curr.value)
+        curr = curr.next;
+    }
 }
 
 let linkedList = new myLinkedList;
@@ -59,6 +90,10 @@ linkedList.addAtHead(4);
 linkedList.addAtHead(5);
 linkedList.addAtTail(6);
 linkedList.addAtTail(2);
-linkedList.addAtIndex(-1,10);
+linkedList.addAtIndex(1,10);
 
+//console.log(linkedList.get(0))
+linkedList.print();
+linkedList.deleteNode(0);
 console.log(linkedList,"new Linked List")
+linkedList.print();
